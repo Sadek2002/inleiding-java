5fe1751e7067f66c256f09fc75a6a4d8a562f864
Sadek2002 <sharkalmousawi@live.com>
2019-09-06 11:36:44 +0200

    test commit

--- src/H4/Praktijk.java
+++ src/H4/Praktijk.java
@@ -15,5 +15,23 @@ public class Praktijk extends Applet {
         g.drawString("Lijn", 125, 40);
         g.drawRect(10, 50, 260, 130);
         g.drawString("Rechthoek", 105, 200);
+        g.setColor(Color.magenta);
+        g.fillRect(290, 50, 260, 130);
+        g.setColor(Color.black);
+        g.drawArc(290, 50, 260, 130, 90, 360);
+        g.drawString("Gevulde rechthoek met ovaal", 350, 200);
+        g.drawArc(570, 50, 260, 130, 90, 360);
+        g.setColor(Color.magenta);
+        g.fillArc(570, 50, 260, 130, 0, 45);
+        g.setColor(Color.black);
+        g.drawString("Taartpunt met ovaal eromheen", 620, 200);
+        g.drawRoundRect(10, 210, 260, 130, 10, 10);
+        g.drawString("Afgeronde rechthoek", 80, 360);
+        g.setColor(Color.magenta);
+        g.fillOval(290, 210, 260, 130);
+        g.setColor(Color.black);
+        g.drawString("Gevulde ovaal", 380, 360);
+        g.drawArc(650, 210, 120, 120, 90, 360);
+        g.drawString("Cirkel", 695, 360);
     }
 }
